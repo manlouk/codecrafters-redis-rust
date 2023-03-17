8299c5ca314d3110a7f41d5696b3b196c0d6c5d4
Manos <e.loukadakis@southpole.com>
2023-03-17 16:41:14 +0000

    PING implemented

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,39 @@
+use std::io::Write;
 //Uncomment this block to pass the first stage
 use std::net::TcpListener;
+use std::net::TcpStream;
+use std::io;
+use std::io::Result;
 
-fn main() {
+
+fn handle_connection(stream: &mut TcpStream){
+
+    let response = b"PONG";
+
+    let mut writer = io::BufWriter::new(stream);
+
+    println!("{:?}",writer.write(response).unwrap());
+    writer.flush().unwrap();
+    
+   
+  
+}
+
+fn main()-> Result<()>{
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
 
     //ncomment this block to pass the first stage
     
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
-    
+   
     for stream in listener.incoming() {
-        match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
-            }
-            Err(e) => {
-                println!("error: {}", e);
-            }
-        }
+        match stream{
+            Ok(mut _stream) =>{
+               handle_connection(&mut _stream);
+            },
+            Err(_stream) => println!("FAILED")
+          };
     }
+    Ok(())
 }
