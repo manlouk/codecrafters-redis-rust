a01fb33c71857d68f20ebfd769cea9d53ab8e223
Manos <e.loukadakis@southpole.com>
2023-03-18 11:44:22 +0000

    implement multiple pings in one connection

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+use std::io::BufRead;
+use std::io::BufReader;
+use std::io::BufWriter;
+use std::io::Read;
 use std::io::Write;
 //Uncomment this block to pass the first stage
 use std::net::TcpListener;
@@ -10,10 +14,49 @@ fn handle_connection(stream: &mut TcpStream){
 
     let response = b"+PONG\r\n";
 
+    println!("Inside handle");
+
+    let stream_copy = stream.try_clone().unwrap();
+    
+    let mut writer = BufWriter::new(stream_copy);
+
+    let mut reader = BufReader::new(stream);
+    
+    
+
+    println!("{:?}",reader);
+
+    for line in reader.lines(){
+        match line{
+            Ok(_line) => {
+                println!("{}",_line);
+                let new_line = _line.replace("*", "").parse().unwrap();
+                for i in 0..new_line{
+                    writer.write_all(b"PONG");
+                
+                }
+                writer.flush();
+                break;
+            }
+            Err(_line) => panic!("No valid input")
+        }
+        
+
+    }
+   
+    
+   
+    
+   
+    // let mut buf = String::new();
+    // // reader.read_to_string(&mut buf);
+
+    // println!("{:?}", buf);
+
     // let mut writer = io::BufWriter::new(stream);
 
-    stream.write(response).unwrap();
-    stream.flush().unwrap();
+    // stream.write(response).unwrap();
+    // stream.flush().unwrap();
     
    
   
@@ -28,6 +71,7 @@ fn main()-> Result<()>{
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
    
     for stream in listener.incoming() {
+        println!("{:?}", stream);
         match stream{
             Ok(mut _stream) =>{
                handle_connection(&mut _stream);
